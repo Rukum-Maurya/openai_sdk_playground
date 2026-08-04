cefe4ca8e6417673c52a23deed75928e13fe65db
Rukum Maurya <rukummaurya@gmail.com>
2026-08-05 01:51:16 +0530

    Add tokenizer analysis and cost estimation utilities

--- modules/tokenizer.py
+++ modules/tokenizer.py
@@ -11,4 +11,12 @@ class Tokenizer:
 
     def count_tokens(self, text):
         return len(self.encode(text))
-    
+
+    def compare(self,text):
+        return {
+            "characters": len(text),
+            "words": len(text.split()),
+            "tokens": self.count_tokens(text)
+        }
+    def estimated_cost(self,text,price_per_million_tokens):
+        return ((self.count_tokens(text) / 1000000) * price_per_million_tokens)
